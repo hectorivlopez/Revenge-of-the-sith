2c3b323d082981f61f5466a4d8e9b9628bc7d968
Hector Lopez <hectorivlopez@gmail.com>
2024-06-24 17:52:43 -0600

    calPolyhedronfaces added

--- src/graphics/Draw3d.java
+++ src/graphics/Draw3d.java
@@ -377,6 +377,60 @@ public class Draw3d {
         drawSortedSurfaces(allSurfaces, director, projection, buffer);
     }
 
+    public static void calPolyhedronFaces(int[][][] levels, int[] ignoreFaces, int direction, ArrayList<Surface> allSurfaces, int[] director, String projection, Color borderColor, Color color/*, HashMap<Color, int[]> colors*/, BufferedImage buffer) {
+        int numLevels = levels.length;
+        int numVertices = levels[0][0].length;
+
+        // All faces
+        int numFaces = numVertices * (numLevels - 1) + 2;
+
+        // Upside and down faces
+        int[][] initFace = levels[0];
+        int[][] finalFace = levels[numLevels - 1];
+
+        int ignoreIndex = 0;
+
+        if(ignoreFaces != null && ignoreIndex < ignoreFaces.length && ignoreFaces[ignoreIndex] == 0) {
+            ignoreIndex++;
+        }
+        else {
+            Surface initSurface = new Surface(initFace, -1 * direction, borderColor, color);
+            allSurfaces.add(initSurface);
+        }
+        if(ignoreFaces != null && ignoreIndex < ignoreFaces.length && ignoreFaces[ignoreIndex] == 1) {
+            ignoreIndex++;
+        }
+        else {
+            Surface finalSurface = new Surface(finalFace, 1 * direction, borderColor, color);
+            allSurfaces.add(finalSurface);
+        }
+
+        for(int i = 0; i < numLevels - 1; i++) {
+            for(int j = 0; j < numVertices; j++) {
+                int faceNum = (i * numVertices) + j + 2;
+                if(ignoreFaces != null && ignoreIndex < ignoreFaces.length && ignoreFaces[ignoreIndex] == faceNum) {
+                    ignoreIndex++;
+                    continue;
+                }
+
+                int[][] sideFace;
+                int next = j < numVertices - 1 ? j + 1 : 0;
+
+                sideFace = new int[][]{
+                        new int[]{levels[i][0][j], levels[i][0][next], levels[i + 1][0][next], levels[i + 1][0][j]},
+                        new int[]{levels[i][1][j], levels[i][1][next], levels[i + 1][1][next], levels[i + 1][1][j]},
+                        new int[]{levels[i][2][j], levels[i][2][next], levels[i + 1][2][next], levels[i + 1][2][j]},
+                };
+
+                Surface surface = new Surface(sideFace, 1 * direction, borderColor, color);
+                allSurfaces.add(surface);
+            }
+        }
+
+
+    }
+
+
     public static boolean backFaceCulling(int[][] face, int[] centroid, int[] director, String projection, double direction) {
         double[] perpendicularVector = Utils.calculatePerpendicularVector(face[0], face[1], face[2], 1, direction);
 
